f2cf45c7d0b25dcfe872db52d155094d13f85647
John DoeIvanDF <delfatti.ivan@gmail.com>
2021-01-19 14:43:09 +0100

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Car;
+
+class HomeController extends Controller
+{
+    public function index() 
+    {
+
+        // Get data from db
+        $cars = Car::all();
+        dump($cars);
+
+
+        return view('home', compact('cars'));
+    }
+}
---
+++ resources/views/partials/main-header.blade.php
@@ -0,0 +1,14 @@
+<header class="mainHeader">
+    <section class="hero is-primary">
+        <div class="hero-body">
+            <div class="container">
+            <h1 class="title">
+                Laravel
+            </h1>
+            <h2 class="subtitle">
+                Model & Migration
+            </h2>
+            </div>
+        </div>
+    </section>
+</header>
